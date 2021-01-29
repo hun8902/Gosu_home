2a0eea2bf367e5de3f495f3b138042a945376923
younguhnkim <hun8902@naver.com>
2021-01-29 14:59:33 +0900

    배너부분 슬라이드 작업

--- js/main.js
+++ js/main.js
@@ -19,23 +19,6 @@ $(document).ready(function() {
   $(this).siblings('label').text(select_name);
   });
 });
-new Swiper('.swiper-container', {
-
-	slidesPerView : 3, // 동시에 보여줄 슬라이드 갯수
-	spaceBetween : 30, // 슬라이드간 간격
-	slidesPerGroup : 3, // 그룹으로 묶을 수, slidesPerView 와 같은 값을 지정하는게 좋음
-
-	// 그룹수가 맞지 않을 경우 빈칸으로 메우기
-	// 3개가 나와야 되는데 1개만 있다면 2개는 빈칸으로 채워서 3개를 만듬
-	loopFillGroupWithBlank : true,
-
-	loop : true, // 무한 반복
-
-	navigation : { // 네비게이션
-		nextEl : '.swiper-button-next', // 다음 버튼 클래스명
-		prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
-	},
-});
 
 
 $('.open-popup-link').magnificPopup({
@@ -66,3 +49,23 @@ var mySwiper = new Swiper('.swiper-container', {
 })
 
 
+
+var swiper = new Swiper('.swiper-container_banner', {
+  slidesPerView: 7,
+  spaceBetween: 0,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
