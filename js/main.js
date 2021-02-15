181cdcec23a97a4176aa801fa1d597621d4020b7
younguhnkim <hun8902@naver.com>
2021-02-15 16:20:59 +0900

    html5 -> jquery로 수정작업

--- js/main.js
+++ js/main.js
@@ -1,34 +1,4 @@
-$(document).ready(function() {
 
-    $('#top_gnb').DB_naviFullWideMove({
-        key:'c37080',                 //라이센스키
-        pageNum:3,           //메인메뉴 페이지인식(1~)
-        subNum:1,            //서브메뉴 페이지인식(1~)
-        subState:false,         //메뉴활성시 서브메뉴의 오픈유무(true,false)
-        motionSpeed:300,        //모션속도(밀리초)
-        delayTime:100             //메뉴아웃시 되돌아가는 딜레이시간(밀리초)
-    });
-
-  var selectTarget = $('.selectbox select');
- selectTarget.on('blur', function(){
-   $(this).parent().removeClass('focus');
- });
-  selectTarget.change(function(){
-    var select_name = $(this).children('option:selected').text();
-  $(this).siblings('label').text(select_name);
-  });
-});
-new SwitchMenu('#mySubMenu', ['#subpage1', '#subpage2', '#subpage3', '#subpage4', '#subpage5']);
-
-$('.open-popup-link').magnificPopup({
-  type:'inline',
-  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-});
-// 애니메이션
-$('.animate').scrolla({
-	mobile: true,
-	once: true
-});
 
 var swiper = new Swiper('.indexvisual-container', {
   // Optional parameters
@@ -80,6 +50,45 @@ var mySwiper = new Swiper('.swiper-container', {
 
 });
 
+var swiper = new Swiper('.swiper-container_photo', {
+  slidesPerView: 7,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+var swiper = new Swiper('.swiper-container_banner', {
+  slidesPerView: 7,
+  spaceBetween: 0,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
 var swiper = new Swiper('.youtube-container', {
   // Optional parameters
@@ -129,42 +138,52 @@ function zoomOut(event) {
   event.target.style.transition = "all 0.5s";
 };
 
-var swiper = new Swiper('.swiper-container_photo', {
-  slidesPerView: 7,
-  spaceBetween: 30,
-  slidesPerGroup: 1,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+//new SwitchMenu('#mySubMenu', ['#subpage1', '#subpage2', '#subpage3', '#subpage4', '#subpage5']);
+$('.open-popup-link').magnificPopup({
+  type:'inline',
+  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+});
+// 애니메이션
+$('.animate').scrolla({
+	mobile: true,
+	once: true
 });
 
-var swiper = new Swiper('.swiper-container_banner', {
-  slidesPerView: 7,
-  spaceBetween: 0,
-  slidesPerGroup: 1,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+
+
+$(document).ready(function() {
+  //When page loads...
+  $(".tab_content").hide(); //Hide all content
+  $("ul.tabs li:first").addClass("active").show(); //Activate first tab
+  $(".tab_content:first").show(); //Show first tab content
+
+  //On Click Event
+  $("ul.tabs li").click(function() {
+
+      $("ul.tabs li").removeClass("active"); //Remove any "active" class
+      $(this).addClass("active"); //Add "active" class to selected tab
+      $(".tab_content").hide(); //Hide all tab content
+
+      var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+      $(activeTab).fadeIn(); //Fade in the active ID content
+      return false;
+  });
+  
+  $('#top_gnb').DB_naviFullWideMove({
+      key:'c37080',                 //라이센스키
+      pageNum:3,           //메인메뉴 페이지인식(1~)
+      subNum:1,            //서브메뉴 페이지인식(1~)
+      subState:false,         //메뉴활성시 서브메뉴의 오픈유무(true,false)
+      motionSpeed:300,        //모션속도(밀리초)
+      delayTime:100             //메뉴아웃시 되돌아가는 딜레이시간(밀리초)
+  });
+
+  var selectTarget = $('.selectbox select');
+  selectTarget.on('blur', function(){
+        $(this).parent().removeClass('focus');
+  });
+  selectTarget.change(function(){
+    var select_name = $(this).children('option:selected').text();
+  $(this).siblings('label').text(select_name);
+  });
 });
