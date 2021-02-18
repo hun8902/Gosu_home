87c820019ae5a860b58e8d8477e70e01764256a8
younguhnkim <hun8902@naver.com>
2021-02-18 17:37:18 +0900

    210218

---
+++ footer.php
@@ -0,0 +1,34 @@
+<div class="foot_link">
+			<div class="container">
+				<ul class="foot_ul">
+					<li><a href="#">회사소개</a></li>
+					<li><a href="#">개인정보처리방침</a></li>
+					<li><a href="#">이용약관</a></li>
+					<li><a href="#">금융사기 관련 유의사항</a></li>
+				</ul>
+			</div>
+		</div>
+		<!-- footer  -->
+		<div class="foot">
+			<div class="container">
+				<div class="copy_1">
+					주식회사 고수경제티브이 / 대표자 조덕진 ㅣ 대표전화 1566-2827 / 1899-5286 ㅣ FAX 02-332-3210<br/>
+					이메일 gosuecotv@naver.com ㅣ 본사: 서울시 마포구 서교동 395-155, 4층<br/>
+					사업자 등록번호 416-86-03623 ㅣ 통신판매업신고번호 : 제2019-서울마포-2605호<br/>
+					계좌번호 : 국민은행 479001-01-285955 (주)고수경제티브이 <br/>
+					
+					<span>Copyrightⓒ GOSUECOTV Co.,Ltd all right served </span>
+				</div>
+				<div class="copy_2">
+					<div class="foot_selectbox">
+						<label for="site_select">패밀리사이트 및 유관기관</label>
+						<select id="site_select">
+							<option selected>패밀리사이트 및 유관기관 </option>
+							<option>고수경제 TV</option>
+						</select>
+					</div>
+					<div class="foot_logo"></div>
+				</div>
+			</div>
+		</div>
+		<!-- footer 끝 -->
---
+++ head.php
@@ -0,0 +1,69 @@
+<!-- 탑 메뉴 -->
+<div class="top_select">
+    <div class="container">
+        <div class="selectbox">
+            <label for="lang">KR</label>
+            <select id="lang">
+                <option selected>KR</option>
+                <option>EN</option>
+            </select>
+        </div>
+        <ul class="ts_right">
+            <li><a href="#">로그인</a></li>
+            <li><a href="#">회원가입</a></li>
+        </ul>
+        
+
+    </div>
+</div>
+<!-- 탑 메뉴 끝 -->
+
+<!--nav-->
+<div id="top_gnb">
+    <div class="DB_wrap">
+        <h1><a href="#"><img src="images/logo.png" alt="logo"/></a></h1>
+        <ul class="DB_main">					
+            <li><a href="#">회사소개</a></li>
+            <li><a href="#">CH고수TV</a></li>
+            <li><a href="#">상품안내</a></li>
+            <li><a href="#">성과인증</a></li>
+            <li><a href="#">고객센터</a></li>
+            <li class="redbox"><a href="#">급등무료체험</a></li>
+        </ul>
+    </div>
+    <div class="DB_mask">
+        <div class="DB_move">
+            <div class="DB_subSet">
+                <ul>
+                    <li><a href="#">회사소개</a></li>
+                    <li><a href="#">기업철학</a></li>
+                    <li><a href="#">위험관리체계</a></li>
+                    <li><a href="#">솔루션</a></li>
+                    <li><a href="#">수상내역</a></li>
+                    <li><a href="#">오시는길</a></li>
+                </ul>		
+                <ul>
+                    <li><a href="#">종목분석</a></li>
+                    <li><a href="#">경제이슈</a></li>
+                    <li><a href="#">시황분석</a></li>
+                </ul>		
+                <ul>
+                    <li><a href="#">국내주식</a></li>
+                    <li><a href="#">애널리스트 소개</a></li>
+                    <li><a href="#">가입안내</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">압도적인 성과</a></li>
+                    <li><a href="#">투자 공문인증서</a></li>
+                    <li><a href="#">회원 수익 인증</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">1:1 문의하기</a></li>
+                </ul>
+                
+            </div>
+            
+        </div>
+    </div>
+</div>
+<!--/nav-->
