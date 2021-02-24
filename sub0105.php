e907d8cb82e3c2bd95a4803981c0ab7ba16b6df1
younguhnkim <hun8902@naver.com>
2021-02-24 17:59:19 +0900

    210224 변경사항

--- sub0105.php
+++ sub0105.php
@@ -52,9 +52,66 @@
 <div class="sub_content">
     <div class="container_s">
         <div class="subcontent animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s">수상내역</div>
-        <div class="subcont_txt1 animate" data-animate="fadeIn" data-duration="1s" data-delay="0.3s">주식투자의 진짜 고수들이 뭉친 고수경제TV가 2020년 상반기 [2020 한국소비자만족지수],<br/>[대한민국고객만족브랜드대상], [대한민국넘버원대상]의 주식컨설팅 부문 1위 3관왕을 달성했습니다.</div>
-        <ul>
-            <li></li>
+        <div class="subcont_txt1 animate" data-animate="fadeIn" data-duration="1s" data-delay="0.3s">주식투자의 진짜 고수들이 뭉친 고수경제TV가 2020년 상반기 <span class="boldtext">[2020 한국소비자만족지수]</span>,<br/><span class="boldtext">[대한민국고객만족브랜드대상]</span>, <span class="boldtext">[대한민국넘버원대상]</span>의 주식컨설팅 부문 1위 3관왕을 달성했습니다.</div>
+        <ul class="sub0105_ul">
+            <li>
+                <table class="sub0105_table">
+                    <tr>
+                        <td class="one_photo animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s">
+                            <img src="images/sub0105box1_1.png" alt="">
+                        </td>
+                        <td>
+                            <div class="sub0105box_t1 animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s">2020년 대한민국 고객만족 브랜드 대상</div>
+                            <div class="sub0105box_t2 animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s">- 수상부문 : 투자정보분석(증권정보제공) 부문 1위<br/>
+                            - 주관 : 주간동아, G밸리뉴스, 동아일보, 여성동아</div>
+                            <ul class="sub0105_boximg">
+                                <li class="animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s"><img src="images/sub0105box1_2.png" alt=""></li>
+                                <li class="animate" data-animate="fadeIn" data-duration="1s" data-delay="0.3s"><img src="images/sub0105box1_3.png" alt=""></li>
+                                <li class="animate" data-animate="fadeIn" data-duration="1s" data-delay="0.5s"><img src="images/sub0105box1_4.png" alt=""></li>
+                            </ul>
+                        </td>
+                    </tr>
+                </table>
+                
+            </li>
+            <li>
+                <table class="sub0105_table">
+                    <tr>
+                        <td class="one_photo animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s">
+                            <img src="images/sub0105box2_1.png" alt="">
+                        </td>
+                        <td>
+                            <div class="sub0105box_t1 animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s">2020년 대한민국 넘버원 대상</div>
+                            <div class="sub0105box_t2 animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s">- 수상부문 : 금융서비스(금융컨설팅) 부문 1위<br/>- 주관 : 한국미디어마케팅그룹, G밸리뉴스, 조선일보</div>
+                            <ul class="sub0105_boximg">
+                                <li class="animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s"><img src="images/sub0105box2_2.png" alt=""></li>
+                                <li class="animate" data-animate="fadeIn" data-duration="1s" data-delay="0.3s"><img src="images/sub0105box2_3.png" alt=""></li>
+                                <li class="animate" data-animate="fadeIn" data-duration="1s" data-delay="0.5s"><img src="images/sub0105box2_4.png" alt=""></li>
+                            </ul>
+                        </td>
+                    </tr>
+                </table>
+                
+            </li>
+            <li>
+                <table class="sub0105_table">
+                    <tr>
+                        <td class="one_photo animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s">
+                            <img src="images/sub0105box3_1.png" alt="">
+                        </td>
+                        <td>
+                            <div class="sub0105box_t1">2020년 대한민국 고객만족 브랜드 대상</div>
+                            <div class="sub0105box_t2">- 수상부문 : 정보서비스(증권정보제공) 부문 1위<br/>- 주관 : 한경비즈니스, G밸리뉴스</div>
+                            <ul class="sub0105_boximg">
+                                <li class="animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s"><img src="images/sub0105box3_2.png" alt=""></li>
+                                <li class="animate" data-animate="fadeIn" data-duration="1s" data-delay="0.3s"><img src="images/sub0105box3_3.png" alt=""></li>
+                                <li class="animate" data-animate="fadeIn" data-duration="1s" data-delay="0.5s"><img src="images/sub0105box3_4.png" alt=""></li>
+                            </ul>
+                        </td>
+                    </tr>
+                </table>
+                
+            </li>
         </ul>
      
         
---
+++ sub0106.php
@@ -0,0 +1,70 @@
+<?php include "top.php" ?>
+
+
+<div class="sub_visual sub01 type">
+    <div class="container_s">
+        <div class="subvisual_txt1 animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s">회사소개</div>
+        <div class="subvisual_txt2 animate" data-animate="fadeIn" data-duration="1s" data-delay="0.3s">수익의 창조와 도전을 멈추지 않는 고수경제TV입니다.</div>
+    </div>
+</div>
+
+<div class="content_nav">
+    <div class="container_s">
+        <ul class="center_nav">
+            <li class="home">
+                <a href="#"><img src="images/select_home.png"></a>
+            </li>
+            <li class="select_area">
+                <div class="selectbox_s">
+                    <dl id="center_nav1" class="dropdown">
+                        <dt><a href="#" ><span>신상품순</span></a></dt>
+                        <dd>
+                        <ul class="dropdown2">
+                            <li><a href="#">신상품순</a></li>
+                            <li><a href="#">인기상품순</a></li>
+                            <li><a href="#">낮은가격순</a></li>
+                            <li><a href="#">높은가격순</a></li>
+                        </ul>
+                        </dd>
+                    </dl>
+                </div>
+            </li>
+            <li class="select_area">
+                <div  class="selectbox_s">
+                    <dl  id="center_nav2" class="dropdown">
+                        <dt><a href="#" ><span>신상품순</span></a></dt>
+                        <dd>
+                        <ul class="dropdown2">
+                            <li><a href="#">신상품순</a></li>
+                            <li><a href="#">인기상품순</a></li>
+                            <li><a href="#">낮은가격순</a></li>
+                            <li><a href="#">높은가격순</a></li>
+                        </ul>
+                        </dd>
+                    </dl>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+</div>
+
+<div class="sub_content">
+    <div class="container_s">
+        <div class="subcontent animate" data-animate="fadeIn" data-duration="1s" data-delay="0.1s">오시는 길</div>
+        <div class="subcont_txt1 animate" data-animate="fadeIn" data-duration="1s" data-delay="0.3s">고수경제TV로 오시는 길을 안내합니다.</div>
+        <div class="maps_box">
+            지도자리
+        </div>
+        <ul class="maps_bottom">
+            <li><div class="maps_txtbox">본사</div></li>
+            <li>
+                
+                <div class="maps_txt1">서울시 마포구 서교동 395-155, 4층</div><br/>
+                <div class="maps_txt2">1566-2827 /  1899-5286</div>
+            </li>
+        </ul>
+    </div>
+</div>
+				
+<?php include "bottom.php" ?>
